824fc7dfad9bed141b8811bff5f59bbdda805a65
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2025-01-07 12:38:11 +0100

    Navigate between Game Options and settings

--- minesweeper-game/src/app/game-options/game-options.component.ts
+++ minesweeper-game/src/app/game-options/game-options.component.ts
@@ -1,22 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-game-options',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-options.component.html',
   styleUrl: './game-options.component.scss'
 })
 export class GameOptionsComponent {
 
+  public hideOptions: boolean = false
+
   constructor(private router: Router){}
 
   public startGame(){
+    this.hideOptions = true
     this.router.navigate(['/play'])
   }
 
   public goToSettings(){
+    this.hideOptions = true
     this.router.navigate(['/game-settings']);
   }
 }
--- minesweeper-game/src/app/game-settings/game-settings.component.ts
+++ minesweeper-game/src/app/game-settings/game-settings.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-settings.component.html',
   styleUrl: './game-settings.component.scss'
 })
 export class GameSettingsComponent {
+
+  public hideOptions: boolean = false
+
+  constructor(private router: Router){}
+
+  public confirm(){
+    /* Make some changes before going back to home-options */
+    this.goBackToHomeOptions()
+  }
+
+  public cancel(){
+    this.goBackToHomeOptions()
+  }
+
+  private goBackToHomeOptions(){
+    this.router.navigate(['home-options'])
+  }
 
 }
