75ab58f5a74102084c9098fb21dd1afa5ce9bd8c
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2025-01-07 11:19:36 +0100

    Added some routes

--- minesweeper-game/src/app/app.routes.ts
+++ minesweeper-game/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { GameSettingsComponent } from './game-settings/game-settings.component';
 
 export const routes: Routes = [
-  { path: '/', component: AppComponent},
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'start', component: AppComponent},
+  { path: 'game', component: GameComponent},
+  { path: 'settings', component: GameSettingsComponent},
+  { path: '', redirectTo: '/start', pathMatch: 'full' },
 ];
