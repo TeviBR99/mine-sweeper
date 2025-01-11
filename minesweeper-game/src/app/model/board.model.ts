5d49001cc9f45e6a812cce3b0e7434a9a2472885
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2025-01-11 18:32:48 +0100

    Refactored the code

--- minesweeper-game/src/app/model/board.model.ts
+++ minesweeper-game/src/app/model/board.model.ts
@@ -27,13 +27,14 @@ export class Board{
     const min = 0
     const max = this.boardSize-1
     let utilsModel : RandomIntUtils = new RandomIntUtils(min, max)
-    let mineSet = 0;
 
-    while(mineSet < this.minesNumber){
+    for(let mineSet = 0; mineSet < this.minesNumber; ){
       const xAxys = utilsModel.getRandomInt()
       const yAxys = utilsModel.getRandomInt()
       if(!this.isThereMine(xAxys, yAxys)){
-        this.rows[xAxys][yAxys].mine = true
+        const cell = this.rows[xAxys][yAxys]
+        cell.mine = true
+        cell.neighborMines = -1
         mineSet++
       }
     }
@@ -43,7 +44,10 @@ export class Board{
   private setNeighbourMines(){
     for(let xAxys=0; xAxys<this.boardSize; xAxys++){
       for(let yAxys=0; yAxys<this.boardSize; yAxys++){
-       this.rows[xAxys][yAxys].neighborMines = this.minesAround(xAxys, yAxys)
+        const cell = this.rows[xAxys][yAxys]
+        if(cell.neighborMines > -1){
+          cell.neighborMines = this.minesAround(xAxys, yAxys)
+        }
       }
     }
   }
@@ -85,24 +89,39 @@ export class Board{
 
   public openCells(x: number, y: number){
     const isBorder = x === 0 || x === this.boardSize-1
+    let skip : boolean = false
+    console.log("Coordinates: ", x, ",", y)
 
+    //Upper rows
     for(let i=x; i>=0; i--){
-      this.open(isBorder ? x : i, y, true)
-      this.open(isBorder ? x : i, y, false)
+      skip = this.open(isBorder ? x : i, y, true)
+      skip = this.open(isBorder ? x : i, y, false)
+      if(skip){
+        console.log("Upper rows: ", i)
+        break;
+      }
     }
 
+    //Lower rows
     for(let i=x; i<this.boardSize; i++){
-      this.open(isBorder ? x : i, y, true)
-      this.open(isBorder ? x : i, y, false)
+      skip = this.open(isBorder ? x : i, y, true)
+      skip = this.open(isBorder ? x : i, y, false)
+
+      if(skip){
+        console.log("Lower rows: ", i)
+        break;
+      }
     }
   }
 
   private open(rowIndex: number, cellIndex: number, leftCheck: boolean){
+    let stopped: boolean = false
     const cell : Cell = this.rows[rowIndex][cellIndex]
-    if(cellIndex === 0 || cellIndex === this.boardSize-1 || cell.neighborMines > 0){
-      if(!cell.mine ){
+    // console.log("cell: ", cell)
+    if(cellIndex === 0 || cellIndex === this.boardSize-1 || cell.neighborMines >= 0){
+      if(!cell.mine){
         this.changeCellState(rowIndex, cellIndex, CellState.OPENED)
-        return;
+        stopped = cell.neighborMines >= 0
       }
     }else{
       if(!cell.mine){
@@ -110,5 +129,7 @@ export class Board{
         this.open(rowIndex, leftCheck ? cellIndex-1 : cellIndex+1, leftCheck)
       }
     }
+    return stopped;
   }
+
 }
